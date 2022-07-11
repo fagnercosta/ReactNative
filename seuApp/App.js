00ad9d7df38fdb59dc7e5e0835996c3cb47d33ec
Fagner Costa <2192525@ifam>
2022-07-11 18:08:37 -0400

    Ajuste Ignore

---
+++ seuApp/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+ import React from 'react';
+ import { View,Text } from 'react-native';
+
+
+
+
+
+
+const App: () => Node = () => {
+ 
+ 
+
+  return (
+      <View>
+          <Text>Ola</Text>
+      </View>
+  );
+
+  
+};
